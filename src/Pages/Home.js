9790be5119a52d04722c6324e420fba4791dc086
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-07-05 15:22:53 +0100

    Initial commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -37,12 +37,12 @@ const Home = () => {
       </div>
 
       <div className="flex space-x-4">
-        <div className="bg-opacity-70 rounded-xl px-9 py-2 text-center">
+        <div className="bg-zinc-800 bg-opacity-70 rounded-xl px-9 py-2 text-center">
           <p className="text-golden-moon">0 taps left</p>
         </div>
         <div className="bg-zinc-800 bg-opacity-70 rounded-xl px-9 py-2 text-center flex items-center space-x-2">
           <span className="text-yellow-900">⏰</span>
-          <p className="text-yellow-500">01:18:43</p>
+          <p className="text-red-500">01:18:43</p>
         </div>
       </div>
 
@@ -58,7 +58,7 @@ const Home = () => {
 
       <div className="bg-zinc-800 rounded-xl p-2 w-full max-w-md flex text-sm font-normal justify-between items-center py-5">
         <p className="px-3 text-lg">80 <span className="text-golden-moon px-2 text-lg">LAR</span></p>
-        <button className="bg-purple-700 p-2 px-3 rounded-lg">
+        <button className="bg-golden-moon p-2 px-3 rounded-lg">
           Claim
         </button>
       </div>
