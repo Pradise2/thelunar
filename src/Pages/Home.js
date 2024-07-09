e2f5fa346a8a7d50e43246f698a0bc9f05b64e4f
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-07-09 11:04:47 +0100

    Initial commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -174,7 +174,7 @@ const Home = () => {
     <div className="bg-cover  bg-gradient-to-b from-black to-zinc-900 min-h-screen text-white flex flex-col items-center p-4 space-y-4">
       <div className="bg-zinc-800 bg-opacity-70 rounded-lg p-4 w-full max-w-md text-center">
         <p className="text-zinc-500">Your Lunar Tokens</p>
-        <p className="text-4xl font-normal">{userData?.HomeBalance} <span className="text-golden-moon">LAR</span></p>
+        <p className="text-4xl font-normal">{userData?.HomeBalance.toLocaleString()} <span className="text-golden-moon">LAR</span></p>
       </div>
       <div className="text-center">
         <p>Tap, tap, tap! Can’t stop, won’t stop!</p>
@@ -213,7 +213,7 @@ const Home = () => {
         )}
       </div>
       <div className="bg-zinc-800 bg-opacity-70 rounded-xl p-2 w-full max-w-md flex text-sm font-normal justify-between items-center py-5">
-        <p className="px-3 text-xl font-normal">{userData?.TapClaim} <span className="text-golden-moon px-2 text-xl font-normal">LAR</span></p>
+        <p className="px-3 text-xl font-normal">{userData?.TapClaim.toLocaleString()} <span className="text-golden-moon px-2 text-xl font-normal">LAR</span></p>
         <button className="bg-golden-moon p-2 px-3 rounded-lg" onClick={handleClaim}>
           Claim
         </button>
