b00d4a6f31b7a1313b77e9b31ad028d2ca4bd187
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-07-31 19:44:07 +0100

    Add Bot/serviceAccountKey.json to .gitignore

--- src/Pages/Squad.js
+++ src/Pages/Squad.js
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { addUserToSquad, getUserFromSquad, getUserFromHome } from '../utils/firestoreFunctions';
+import { getUserFromFarm } from '../utils/firestoreFunctions';
 import Footer from '../Component/Footer';
 import { ClipLoader } from 'react-spinners';
 import './bg.css';
-import RCSquad from '../Component/RCSquad';
 import { motion, AnimatePresence } from 'framer-motion';
+import axios from 'axios';
 
 const Squad = () => {
   const [copied, setCopied] = useState(false);
   const [userId, setUserId] = useState("743737380");
-  const [squadData, setSquadData] = useState(null);
-  const [homeData, setHomeData] = useState(null);
+  const [username, setUserName] = useState(null);
+  const [userSquad, setUserSquad] = useState(null);
+  const [squads, setSquads] = useState([]);
+  const [FarmData, setFarmData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [showRCSquad, setShowRCSquad] = useState(false);
-
+  const [error, setError] = useState(null); // Define error state
 
   useEffect(() => {
     // Check if Telegram WebApp is available
     if (window.Telegram && window.Telegram.WebApp) {
       const { WebApp } = window.Telegram;
-      
+
       // Expand the WebApp
       WebApp.expand();
-  
+
       const user = WebApp.initDataUnsafe?.user;
       if (user) {
         setUserId(user.id);
+        setUserName(user.username);
       } else {
         console.error('User data is not available.');
       }
@@ -37,10 +40,17 @@ const Squad = () => {
   useEffect(() => {
     const fetchSquadData = async () => {
       try {
-        const data = await getUserFromSquad(userId);
-        setSquadData(data);
+        const response = await axios.get(`https://lunarapp.thelunarcoin.com/backend/api/squad/${userId}`);
+        const { userSquad, squads } = response.data;
+        setUserSquad(userSquad);
+        setSquads(squads || []); // Ensure squads is an array
+        console.log('Fetched squad data:', { userSquad, squads }); // Log the data
       } catch (error) {
         console.error('Error fetching squad data:', error);
+        setError('Error fetching squad data');
+        // Consider implementing an error retry mechanism or user feedback
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -50,19 +60,19 @@ const Squad = () => {
   }, [userId]);
 
   useEffect(() => {
-    const fetchHomeData = async () => {
+    const fetchFarmData = async () => {
       try {
-        const data = await getUserFromHome(userId);
-        setHomeData(data);
+        const data = await getUserFromFarm(userId);
+        setFarmData(data);
       } catch (error) {
-        console.error('Error fetching home data:', error);
+        console.error('Error fetching farm data:', error);
       } finally {
         setLoading(false);
       }
     };
 
     if (userId) {
-      fetchHomeData();
+      fetchFarmData();
     }
   }, [userId]);
 
@@ -92,45 +102,37 @@ const Squad = () => {
     }
   };
 
-  const calculateTotal = () => {
-    if (homeData && squadData) {
-      return homeData.HomeBalance + squadData.totalBalance;
-    }
-    return 0;
-  };
-
   const handleClaim = async () => {
-    if (squadData?.referralEarnings > 0) {
     // Vibrate when claiming
     if (navigator.vibrate) {
       navigator.vibrate(500); // Vibrate for 500ms
     }
 
-    const updatedTotalBalance = squadData.totalBalance + squadData.referralEarnings;
-    const updatedSquadData = {
-      ...squadData,
-      totalBalance: updatedTotalBalance,
-      referralEarnings: 0
-    };
+    try {
+      const response = await axios.get(`https://lunarapp.thelunarcoin.com/backend/api/squad/update/${userId}`);
+      console.log('Claim response:', response.data);
+      // Update the user squad data after the claim
+      setUserSquad(prevState => ({
+        ...prevState,
+        totalBalance: response.data.totalBalance,
+        claimedReferral: response.data.claimedReferral,
+      }));
+    } catch (error) {
+      console.error('Error during claim:', error);
+      setError('Error during claim');
+      return;
+    }
 
-    setSquadData(updatedSquadData);
     setShowRCSquad(true);
 
-      
+    // Hide RewardCard after 2 seconds
+    setTimeout(() => setShowRCSquad(false), 2000);
 
-      // Hide RewardCard after 2 seconds
-      setTimeout(() => setShowRCSquad(false), 2000);
-
-    try {
-      await addUserToSquad(userId, updatedSquadData);
-    } catch (error) {
-      console.error('Error updating squad data:', error);
-    }
-  }
-};
+   
+  };
 
   if (loading) {
-       return (
+    return (
       <div className="min-h-screen flex justify-center items-center bg-cover text-white p-4">
         <div className="flex flex-col items-center space-y-4">
           <h1 className="text-white text-4xl font-normal">
@@ -143,75 +145,68 @@ const Squad = () => {
         </div>
       </div>
     );
-  } return (
-    <div className="min-h-screen bg-cover text-white  flex flex-col items-center p-4 space-y-4">
+  }
+
+  return (
+    <div className="min-h-screen bg-cover text-white flex flex-col items-center p-4 space-y-4">
       <h1 className="text-center text-4xl font-normal">
-        The larger the tribe, <br /> the greater the experience!
+        The bigger the tribe, <br /> the better the vibe!
       </h1>
-      <div className="bg-zinc-950 bg-opacity-70 p-4 rounded-xl w-full max-w-md space-y-2">
+      <div className="bg-zinc-800 bg-opacity-70 p-4 rounded-xl w-full max-w-md space-y-2">
         <p className="text-zinc-400 text-center">Total squad balance</p>
         <p className="text-center text-3xl font-normal">
-          {calculateTotal().toLocaleString()} <span className="text-golden-moon">LAR</span>
+          {} <span className="text-golden-moon">LAR</span>
         </p>
       </div>
-      <div className="bg-zinc-950 bg-opacity-70 p-4 rounded-xl w-full max-w-md space-y-2">
+      <div className="bg-zinc-800 bg-opacity-70 p-4 rounded-xl w-full max-w-md space-y-2">
         <p className="text-zinc-400 text-center">Your rewards</p>
         <p className="text-center text-3xl font-normal">
-          {squadData?.referralEarnings.toLocaleString() || '0'} <span className="text-golden-moon">LAR</span>
-        </p>
-        <p className="text-sm mb-4 text-center">10% of your friends' earnings</p>
+  {userSquad?.referralCount ? `${userSquad.referralCount * 5000} ` : '0 '}
+  <span className="text-golden-moon">LAR</span>
+</p>
+
+        <p className="text-sm mb-4 text-center">Earning through friends invite</p>
         <div className="flex p-1 justify-center">
-          <button className="bg-golden-moon px-4 py-2 rounded-xl" onClick={handleClaim}>Claim</button>
+          <button className="bg-zinc-600 px-4 py-2 rounded-xl">Claim</button>
         </div>
       </div>
-      <div className="bg-zinc-950 bg-opacity-70 p-4 rounded-xl w-full max-w-md space-y-2">
+      <div className="bg-zinc-800 bg-opacity-70 p-4 rounded-xl w-full max-w-md space-y-2">
         <div className="flex justify-between bg-opacity-70 text-sm items-center bg-zinc-700 rounded-lg py-2 px-3">
           <p className="flex items-center">
             <img aria-hidden="true" alt="team-icon" src="https://openui.fly.dev/openui/24x24.svg?text=👥" className="mr-2" />
             Your team
           </p>
-          <p>{squadData?.referralCount || '0'} users</p>
+          <p>{userSquad?.referralCount} users</p>
         </div>
         <div>
-          {squadData?.referrals.map((referral, index) => (
-            <div key={index} className="flex font-normal text-sm  justify-between items-center px-3">
-              <p className="flex items-center">
-                <img aria-hidden="true" alt="user-icon" src="https://openui.fly.dev/openui/24x24.svg?text=👤" className="mr-2" />
-                {referral.username}
-              </p>
-              <p className="text-primary">{referral.referralBonus.toLocaleString()} <span className="text-golden-moon">LAR</span></p>
-            </div>
-                    )) || <p className="text-center text-sm text-zinc-400">No referrals yet.</p>}
+          {squads.length > 0 ? (
+            squads.map((referral, index) => (
+              <div key={index} className="flex font-normal text-sm justify-between items-center px-3">
+                <p className="flex items-center">
+                  <img aria-hidden="true" alt="user-icon" src="https://openui.fly.dev/openui/24x24.svg?text=👤" className="mr-2" />
+                  {referral.newUserName}
+                </p>
+                <p className="text-primary">{referral.referralEarning} <span className="text-golden-moon">LAR</span></p>
+              </div>
+            ))
+          ) : (  
+            <p className="text-center text-sm text-zinc-400">No referrals yet.</p>
+          )}
         </div>
       </div>
       <div className="w-full max-w-md flex space-x-2 mt-5">
         <button className="flex-1 bg-gradient-to-r from-golden-moon py-2 rounded-lg" onClick={copyToClipboard}>
           Invite friends
         </button>
-        <button className="bg-zinc-950 bg-opacity-70 p-2 rounded-lg" onClick={copyToClipboard}>
+        <button className="bg-zinc-700 bg-opacity-70 p-2 rounded-lg" onClick={copyToClipboard}>
           {copied ? <span>Copied!</span> : <span>Copy</span>}
         </button>
       </div>
-      
-      <AnimatePresence>
-          {showRCSquad && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3 }}
-              className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
-              onClick={() => setShowRCSquad(false)} // Click anywhere to close RewardCard
-            >
-              <RCSquad onClose={() => setShowRCSquad(false)} />
-            </motion.div>
-          )}
-        </AnimatePresence>
-      <div className="w-full sticky max-w-md  bottom-0 left-0 flex text-white bg-zinc-900 justify-around py-1">
+      <div className="w-full max-w-md sticky bottom-0 left-0 flex text-white bg-zinc-900 justify-around py-1">
         <Footer />
       </div>
     </div>
   );
-  };
+};
 
 export default Squad;
